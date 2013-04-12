1f2d41859341786d64fddaee7298b0a373eac89a
LEDUNOIS Simon <simon.ledunois@gmail.com>
2013-04-12 10:44:43 +0000

    update ColorettoPrimitives.h

--- ColorettoPrimitives.h
+++ ColorettoPrimitives.h
@@ -11,16 +11,29 @@
 
 #include "Coloretto.h"
 
-CARD createCard(int colour); //return a card
-CARD* createStock(); //return an array of 77 cards
+/* create and return a card */
+CARD createCard(int colour);
 
-PILE mixCards(); //return a pile
-CARD pickCard(); //return a card and delete it from the pile (PilePop)
+/* create the stock and return an array of 77 cards */
+CARD* createStock();
 
-LISTE* createLine(int nbPlayers); //return an array of pointers (4 à 5)
-void putACard(); //put a card on a line
-PLAYER pickALine(); // return the last player who picked a line
+/* mix the stock and return the pile */
+PILE mixCards(CARD *stock);
 
-PLAYER firstPlayer();//return id of the first player
+/* return a card and delete it from the pile, this function is using pilepop */
+CARD pickCard();
+
+/* creation of the line and return an array of pointers (4 à 5) */
+LISTE* createLine(int nbPlayers);
+
+/* Put a card on a line. this function add the card to the struct line */
+void putACard();
+
+/* return the last player who picked a line */
+PLAYER pickALine();
+
+
+/* return the first player */
+PLAYER firstPlayer();
 
 #endif
